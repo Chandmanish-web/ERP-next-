5bfe5b9d24801fba111e726f726a9b1c263fa46d
Manish Chand <manish2003ban@gmail.com>
2026-03-24 12:32:06 +0530

    Update book_transaction.py

--- library_management/library_management/doctype/book_transaction/book_transaction.py
+++ library_management/library_management/doctype/book_transaction/book_transaction.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2024, Library Management Contributors
-# For license information, please see license.txt
 
 from __future__ import unicode_literals
 import frappe
